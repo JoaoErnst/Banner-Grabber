b09ae12979c34a1889bf3ce6d4e58b4779b6905c
João Ernst <74716479+JoaoErnst@users.noreply.github.com>
2023-12-03 15:55:18 -0300

    Update banner.py

--- banner.py
+++ banner.py
@@ -4,12 +4,12 @@ import socket
 host = input("Digite o nome do host: ")
 portas_str = input("Digite a lista de portas separadas por vírgula: ")
 
-# Divida a string de portas em uma lista
+
 portas = [int(p) for p in portas_str.split(',')]
 
 for porta in portas:
     meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    meusocket.settimeout(1)  # Defina um timeout para a tentativa de conexão
+    meusocket.settimeout(1)  
     try:
         meusocket.connect((host, porta))
         banner = meusocket.recv(1024)
